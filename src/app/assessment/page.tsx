66f116e7850043ce14bc1109e2089c7f3773b01d
sabrananda <sabranandasatria@gmail.com>
2024-04-29 22:07:42 +0700

    rollback

--- src/app/assessment/page.tsx
+++ src/app/assessment/page.tsx
@@ -1,25 +1,20 @@
-"use client"
+"use client";
 import { useEffect } from "react";
-import AssessmentForm from "./assessmentForm";
 import { homeState } from "@/states/homeState";
 import { useRouter } from "next/navigation";
 import Form from "./form";
 
 export default function AssessmentPage() {
-  const router = useRouter()
-  const [nama] = homeState((state) => [state.nama])
+  const router = useRouter();
+  const [nama] = homeState((state) => [state.nama]);
   useEffect(() => {
     if (nama === "") {
-      router.push("/")
+      router.push("/");
     }
-  }, [nama, router])
+  }, [nama, router]);
   return (
     <div>
-      {/* <div className="mb-10 flex justify-end">
-        <button className="font-bold bg-blue-200 rounded-sm px-2 py-1 hover:bg-blue-400" onClick={reset}>reset</button>
-      </div> */}
-      {/* <AssessmentForm /> */}
       <Form />
     </div>
-  )
+  );
 }
